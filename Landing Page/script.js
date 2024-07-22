3b6612fc5a208043bec8f18a22da8c0076efaa88
Thulasi Raman S <146525897+ThulasiRaman03778@users.noreply.github.com>
2024-07-22 16:53:12 +0530

    script.js

--- Landing Page/script.js
+++ Landing Page/script.js
@@ -3,7 +3,7 @@ window.addEventListener('load', function() {
     const loading = document.getElementById('loading');
     setTimeout(() => {
         loading.style.display = 'none';
-    }, 3000);
+    }, 2000);
 });
 
 // Automatically set the current year
